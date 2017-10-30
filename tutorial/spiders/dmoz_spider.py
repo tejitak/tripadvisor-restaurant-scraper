594ed7075eb39acc62341f5f93e2a6f1acb2e6aa
tejitak <tejitak@gmail.com>
2017-10-30 22:24:29 +0700

    use latest chrome driver

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -2,59 +2,58 @@ import scrapy
 from scrapy.http import TextResponse
 from scrapy.selector import Selector
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
+from pyvirtualdisplay import Display
+
 #import items.py
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["tripadvisor.com"]
     start_urls = [
-        "https://www.tripadvisor.com/Restaurants-g293916-Bangkok.html"
+        #"https://www.tripadvisor.com/Restaurants-g304554-Mumbai_Bombay_Maharashtra.html"
+        "https://www.tripadvisor.com/Restaurants-g298184-Tokyo_Tokyo_Prefecture_Kanto.html"
     ]
     def __init__(self):
-        binary = FirefoxBinary('/opt/selenium_firefox/firefox/firefox')
-        self.browser = webdriver.Firefox(firefox_binary=binary)
+        display = Display(visible=0, size=(800, 800))  
+        display.start()
+        self.browser = webdriver.Chrome('/opt/selenium_chrome/chromedriver')
 
     def parse(self, response):
-        #namelist = response.xpath('//*[contains(@class,"property_title")]/text()').extract()[0]
-        #linklist = response.xpath('//*[contains(@class,"property_title")]/@href').extract()[0]
         self.browser.get(response.url)
         time.sleep(5.0)
         body = self.browser.page_source
         sel_response = Selector(text=body)
-        restro_list = sel_response.xpath('//*[@id="EATERY_SEARCH_RESULTS"]/div/div[2]')
+        restro_list = sel_response.xpath('//*[@id="EATERY_SEARCH_RESULTS"]/div[contains(@class, "listing")]')
         l = "https://www.tripadvisor.com"
-            #print len(restro_list)
+        # print('len', len(restro_list))
         f = open('data.csv', 'w+')
         for restro in restro_list:
-            name = restro.xpath('div/div[contains(@class, "title")]/a/text()').extract()[0].replace("\n","")
-            link = l+restro.xpath('div/div[contains(@class, "title")]/a/@href').extract()[0]
-            print name, link
-            #review1 = restro.xpath('ul/li[1]/span/a/text()').extract()[0]
-            #review2 = restro.xpath('ul/li[2]/span/a/text()').extract()[0]
-            f.write(name.encode('utf-8')+","+link.encode('utf-8') + "\n")
+            name = restro.xpath('div[2]/div/div[contains(@class, "title")]/a/text()').extract()[0].replace("\n","")
+            link = l+restro.xpath('div[2]/div/div[contains(@class, "title")]/a/@href').extract()[0]
+            print(name, link)
+            f.write('"' + name.replace('"', '\\"') + '",' + link + "\n")
             f.flush()
-        next = self.browser.find_element_by_xpath('//*[@id="EATERY_LIST_CONTENTS"]/div[3]/div/a')
         window_before = self.browser.window_handles[0]
-        next.click()
+        self.browser.execute_script('document.querySelector("a.nav.next").click();') 
 
         self.browser.switch_to_window(window_before)
         page_count = 0
         while True:
             page_count += 1
-            print "page_count: ", page_count
+            print("page_count: ", page_count)
             time.sleep(5.0)
             body = self.browser.page_source
             sel_response = Selector(text=body)
-            restro_list = sel_response.xpath('//*[@id="EATERY_SEARCH_RESULTS"]/div/div[2]')
+            restro_list = sel_response.xpath('//*[@id="EATERY_SEARCH_RESULTS"]/div[contains(@class, "listing")]')
             l = "https://www.tripadvisor.com"
-            #print len(restro_list)
             for restro in restro_list:
-                name = restro.xpath('div/div[contains(@class, "title")]/a/text()').extract()[0].replace("\n","")
-                link = l+restro.xpath('div/div[contains(@class, "title")]/a/@href').extract()[0]
-                print name, link
-                f.write('"'+name.encode('utf-8')+'","'+link.encode('utf-8')+'"\n')
+                name = restro.xpath('div[2]/div/div[contains(@class, "title")]/a/text()').extract()[0].replace("\n","")
+                link = l+restro.xpath('div[2]/div/div[contains(@class, "title")]/a/@href').extract()[0]
+                print(name, link)
+                f.write('"' + name.replace('"', '\\"') + '",' + link + "\n")
                 f.flush()
             next = self.browser.find_element_by_xpath('//*[@id="EATERY_LIST_CONTENTS"]/div[3]/div/a[2]')
-            next.click()
+            if not next:
+              break
+            self.browser.execute_script('document.querySelector("a.nav.next").click();')
         self.browser.close()
